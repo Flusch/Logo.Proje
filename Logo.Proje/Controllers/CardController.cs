3acb041ee59275bf5731fbd4539e60b1458eafdf
Flusch <yavuzselim.guler@ogr.iuc.edu.tr>
2022-04-20 02:22:43 +0300

    added payment and card views

---
+++ Logo.Proje/Controllers/CardController.cs
@@ -0,0 +1,160 @@
+﻿using Logo.Proje.Business.Abstracts;
+using Logo.Proje.Data;
+using Logo.Proje.Domain.MongoDbEntities;
+using Logo.Proje.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+namespace Logo.Proje.Controllers
+{
+    public class CardController : Controller
+    {
+        private readonly ApplicationDbContext _userContext;
+        private readonly ICardService _cardService;
+        private readonly IMongoCollection<Card> _cards;
+        private readonly UserManager<CustomIdentityUser> _userManager;
+
+        public CardController(ICardService cardService,
+            ApplicationDbContext usercontext,
+            IMongoClient mongoClient,
+            UserManager<CustomIdentityUser> userManager)
+        {
+            _userContext = usercontext;
+            _cardService = cardService;
+            _cards = mongoClient.GetDatabase("LogoDb").GetCollection<Card>("cards");
+            _userManager = userManager;
+
+        }
+
+        // GET: Card
+        public IActionResult Index()
+        {
+            return View(_cardService.GetAllCards());
+        }
+
+        // GET: Card/Details/5
+        public IActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var card = _cardService.GetCardById(x => x.Id == id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+            return View(card);
+        }
+
+        // GET: Card/Create
+        public async Task<IActionResult> CreateAsync()
+        {
+            System.Security.Claims.ClaimsPrincipal currentUser = this.User;
+            var id = _userManager.GetUserId(User);
+            var user = await _userManager.GetUserAsync(User);
+            var email = user.Email;
+
+            ViewData["OwnerId"] = new SelectList(_userContext.Users, "Id", "Email"); //fix: find a way to show the 'fullname(email)' instead of just email
+            return View();
+        }
+
+        // POST: Card/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("CardName,OwnerId,OwnerFullName,CardNumber,ExpirationDate,CVV,Id,IsDeleted,CreatedAt,CreatedBy,LastUpdatedAt,LastUpdatedBy")] Card card)
+        {
+            if (ModelState.IsValid)
+            {
+                _cardService.AddCard(card);
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OwnerId"] = new SelectList(_userContext.Users, "Id", "Email"); //fix: find a way to show the 'fullname(email)' instead of just email
+            return View(card);
+        }
+
+        // GET: Card/Edit/5
+        public IActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var card = _cardService.GetCardById(x => x.Id == id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+            ViewData["OwnerId"] = new SelectList(_userContext.Users, "Id", "Email"); //fix: find a way to show the 'fullname(email)' instead of just email
+            return View(card);
+        }
+
+        // POST: Card/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(string id, [Bind("CardName,OwnerId,OwnerFullName,CardNumber,ExpirationDate,CVV,Id,IsDeleted,CreatedAt,CreatedBy,LastUpdatedAt,LastUpdatedBy")] Card card)
+        {
+            if (id != card.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _cardService.UpdateCard(card);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CardExists(card.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OwnerId"] = new SelectList(_userContext.Users, "Id", "Email"); //fix: find a way to show the 'fullname(email)' instead of just email
+            return View(card);
+        }
+
+        // GET: Card/Delete/5
+        public IActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var card = _cardService.GetCardById(x => x.Id == id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            return View(card);
+        }
+
+        // POST: Card/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(string id)
+        {
+            _cardService.DeleteCard(new Card { Id = id });
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CardExists(string id)
+        {
+            return _cards.Find<Card>(e => e.Id == id).Any();
+        }
+    }
+}
---
+++ Logo.Proje/Controllers/PaymentController.cs
@@ -0,0 +1,160 @@
+﻿using Logo.Proje.Business.Abstracts;
+using Logo.Proje.Data;
+using Logo.Proje.Domain.MongoDbEntities;
+using Logo.Proje.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+namespace Logo.Proje.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly ApplicationDbContext _userContext;
+        private readonly IPaymentService _paymentService;
+        private readonly IMongoCollection<Payment> _payments;
+        private readonly UserManager<CustomIdentityUser> _userManager;
+
+        public PaymentController(IPaymentService paymentService,
+            ApplicationDbContext usercontext,
+            IMongoClient mongoClient,
+            UserManager<CustomIdentityUser> userManager)
+        {
+            _userContext = usercontext;
+            _paymentService = paymentService;
+            _payments = mongoClient.GetDatabase("LogoDb").GetCollection<Payment>("payments");
+            _userManager = userManager;
+
+        }
+
+        // GET: Card
+        public IActionResult Index()
+        {
+            return View(_paymentService.GetAllPayments());
+        }
+
+        // GET: Card/Details/5
+        public IActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var card = _paymentService.GetPaymentById(x => x.Id == id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+            return View(card);
+        }
+
+        // GET: Card/Create
+        public async Task<IActionResult> CreateAsync()
+        {
+            System.Security.Claims.ClaimsPrincipal currentUser = this.User;
+            var id = _userManager.GetUserId(User);
+            var user = await _userManager.GetUserAsync(User);
+            var email = user.Email;
+
+            ViewData["OwnerId"] = new SelectList(_userContext.Users, "Id", "Email"); //fix: find a way to show the 'fullname(email)' instead of just email
+            return View();
+        }
+
+        // POST: Card/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("CardId,BillId,PaymentDate,Id,IsDeleted,CreatedAt,CreatedBy,LastUpdatedAt,LastUpdatedBy")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                _paymentService.AddPayment(payment);
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OwnerId"] = new SelectList(_userContext.Users, "Id", "Email"); //fix: find a way to show the 'fullname(email)' instead of just email
+            return View(payment);
+        }
+
+        // GET: Card/Edit/5
+        public IActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var card = _paymentService.GetPaymentById(x => x.Id == id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+            ViewData["OwnerId"] = new SelectList(_userContext.Users, "Id", "Email"); //fix: find a way to show the 'fullname(email)' instead of just email
+            return View(card);
+        }
+
+        // POST: Card/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(string id, [Bind("CardId,BillId,PaymentDate,Id,IsDeleted,CreatedAt,CreatedBy,LastUpdatedAt,LastUpdatedBy")] Payment payment)
+        {
+            if (id != payment.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _paymentService.UpdatePayment(payment);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CardExists(payment.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OwnerId"] = new SelectList(_userContext.Users, "Id", "Email"); //fix: find a way to show the 'fullname(email)' instead of just email
+            return View(payment);
+        }
+
+        // GET: Card/Delete/5
+        public IActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var card = _paymentService.GetPaymentById(x => x.Id == id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            return View(card);
+        }
+
+        // POST: Card/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(string id)
+        {
+            _paymentService.DeletePayment(new Payment { Id = id });
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CardExists(string id)
+        {
+            return _payments.Find<Payment>(e => e.Id == id).Any();
+        }
+    }
+}
